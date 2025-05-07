286379a7f25dc65a85af239fa0bd3255683e0457
Manoel <manoelrabelo7@gmail.com>
2025-05-06 22:50:20 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/rabelo/manoel/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabelo.manoel.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
